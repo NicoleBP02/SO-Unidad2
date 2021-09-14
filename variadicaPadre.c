afda04ae2eb98a476b36b224b3518bf72dfbf159
Nicole Bruneau <nicole_bruneau@hotmail.com>
2021-09-14 16:27:33 -0500

    reto9 funcionando

--- variadicaPadre.c
+++ variadicaPadre.c
@@ -1,14 +1,17 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-//función Padre 
-int main(int argc, char** argv){
+//función Padre
+int main(int argc, char **argv)
+{
     pid_t pid_hijo;
     pid_t pid_hijo2;
+    pid_t pid_hijo3;
 
     pid_hijo = fork();
-    int ans = 1;
     int status;
 
     switch (pid_hijo)
@@ -16,35 +19,38 @@ int main(int argc, char** argv){
     case -1: /* Error */
         printf("Error al crear el proceso");
         return -1;
-    case 0: /* Código ejecutado por el hijo */
-        for (int i = 1; i < 11; i++)
-        {
-            ans *= i;
-            printf("HIJO1: fact (%d): %d\n", i, ans);
-        }
-    break;
-    default: {/* Código ejecutado por el padre */
+    case 0: ;/* Código ejecutado por el hijo */
+        execv("./variadicaProm",argv);
+        break;
+    default:
+    { /* Código ejecutado por el padre */
 
-            pid_hijo2 = fork();
-            int ans2 = 1;
-            switch (pid_hijo2)
+        pid_hijo2 = fork();
+        switch (pid_hijo2)
+        {
+        case -1: /* Error */
+            printf("Error al crear el proceso");
+            return -1;
+        case 0: ;/* Código ejecutado por el hijo */
+            execv("./variadicaMin",argv);
+            break;
+        default: /* Código ejecutado por el padre */
+            pid_hijo3 = fork();
+            switch (pid_hijo3)
             {
             case -1: /* Error */
                 printf("Error al crear el proceso");
                 return -1;
-            case 0: /* Código ejecutado por el hijo */
-                for (int i = 1; i < 11; i++)
-                {
-                    ans2 *= i;
-                    printf("HIJO2: fact (%d): %d\n", i, ans2);
-                }
-            break;
-            default: /* Código ejecutado por el padre */
+            case 0: ;/* Código ejecutado por el hijo */
+                execv("./variadicaMax",argv);
+                break;
+            default:           /* Código ejecutado por el padre */
                 wait(&status); //espera que el acta de defunción de los hijos zombies
+                wait(&status);
                 wait(&status);
                 printf("El padre terminó\n");
             }
         }
     }
-
+    }
 }
